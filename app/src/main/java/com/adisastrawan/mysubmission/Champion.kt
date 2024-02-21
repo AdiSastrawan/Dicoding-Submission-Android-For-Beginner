eb0698c80fdbd46b057cf22764bab9809f8802cd
AdiSastrawan <adisastrawan110703@gmail.com>
2024-02-21 09:40:56 +0800

    update:adding additional data

--- app/src/main/java/com/adisastrawan/mysubmission/Champion.kt
+++ app/src/main/java/com/adisastrawan/mysubmission/Champion.kt
@@ -8,4 +8,7 @@ data class Champion(
     val name:String,
     val description:String,
     val photo:Int,
+    val difficulty:String = "Low",
+    val role:String,
+    val subname:String
 ):Parcelable
